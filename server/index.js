0c65b96113124f062f70c7c533eb32dab756e93b
Diego Rodrigo Samame Salazar <tsm_dsamame@niubiz.com.pe>
2024-06-11 19:43:30 -0500

    connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 import express from 'express'
 import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT ?? 3000
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecoveryTimeout:{}
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+    socket.on('chat message', (chatMessage) => {
+        console.log('message:' + chatMessage)
+        io.emit('chat message', chatMessage)
+    })
+})
+
 app.use(logger('dev'))
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
@@ -10,6 +32,6 @@ app.get('/', (req, res) => {
 
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 }) 
